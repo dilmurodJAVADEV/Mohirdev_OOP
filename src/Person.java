acd82933b0a4f47c2ced6a586bd5dbb7f93aebd5
Dilmurod <dilmurod10693@yandex.ru>
2025-10-01 22:27:03 +0500

    OOP Task 1 bajarildi

---
+++ src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String gender;
+    private String phone;
+    private String address;
+}
